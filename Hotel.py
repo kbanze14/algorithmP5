bd817187a8d3e438bb23d5bd59e2c26ba62ad3a8
kbanze14 <k.banze@setonhill.edu>
2021-11-26 23:27:13 -0500

    finsihed

--- Hotel.py
+++ Hotel.py
@@ -1,20 +1,6 @@
 class Hotel:
-    # def __init__(self, hotel_location_x, hotel_location_y, num_guests, attendee, name):
-    #     self.hotel_location_x = hotel_location_x
-    #     self.hotel_location_y = hotel_location_y
-    #     self.num_guests = num_guests
-    #     self.attendee = attendee
-    #     self.name = name
-    #
-    # def add_attendee(self, g):
-    #     if self.num_guests < 15:
-    #         self.num_guests += 1
-    #         self.attendee.append(g)
-    #
-    # def print_hlocation(self):
-    #     print(self.name)
-    #     print('Location: ' + str(self.hotel_location_x) + ', ' + str(self.hotel_location_y))
-    #
+
+    # hotel class, pretty self explanatory
     def __init__(self, hotel_x, hotel_y, num_guests, guests, h_name):
         self.hotel_x = hotel_x
         self.hotel_y = hotel_y
--- main.py
+++ main.py
@@ -3,95 +3,41 @@ from Attendee import Attendee
 import random
 import names
 import math
-#
-#
-# def distance(Hotel, Attendee, listy):
-#     dist = math.sqrt(abs(Attendee.attendee_location_x - Hotel.hotel_location_x) + (abs(Attendee.attendee_location_y - Hotel.hotel_location_y)))
-#     listy.append([dist, Hotel.name, Attendee.name])
-#     return listy
-#
-#
-# def hotel_names_list(pref_list, name_list):
-#     for a in range(20):
-#         name_list.append(pref_list[a][1])
-#     return name_list
-#
-#
-# if __name__ == '__main__':
-#     # lists to store all information for making hotels & guests
-#     # hotel location
-#     x_list_hotels = []
-#     y_list_hotels = []
-#     # hotels
-#     hotels = []
-#     #attendees x and y
-#     x_list_attend = []
-#     y_list_attend = []
-#     # attendees
-#     attendees = []
-#     # name generator list
-#     hotel_names = []
-#     # name generator list
-#     attendee_names = []
-#
-#     # random name hen for hotels
-#     for x in range(20):
-#         hotel_names.append('Hotel ' + names.get_first_name(gender='female'))
-#
-#     # random name gen for attendees
-#     for x in range(150):
-#         attendee_names.append(names.get_full_name(gender='male'))
-#
-#     # populating hotel lists
-#     for x in range(20):
-#         x_list_hotels.append(random.randint(0, 200))
-#         y_list_hotels.append(random.randint(0, 200))
-#     for x in range(20):
-#         hotels.append(Hotel(x_list_hotels[x], y_list_hotels[x], 0, [], hotel_names[x]))
-#
-#     # populating guest lists
-#     for x in range(150):
-#         x_list_attend.append(random.randint(0,200))
-#         y_list_attend.append(random.randint(0, 200))
-#     for x in range(150):
-#         attendees.append(Attendee(attendee_names[x], x_list_attend[x], y_list_attend[x], None, None, '', [], []))
-#     list1 = []
-#
-#     # Euclidean distance on each hotel
-#     for x in attendees:
-#         for y in hotels:
-#             x.hotel_pref = distance(y, x, x.hotel_pref)
-#     # sort the list of euclidean distances in order from clsoest to furthest
-#     for x in attendees:
-#         x.sort_prefs()
-#         x.hotel_names_pref = hotel_names_list(x.hotel_pref, x.hotel_names_pref)
-#
-#     attendees[0].print_guest()
-#     print(attendees[0].hotel_pref)
-#     for x in hotels:
-#         x.print_hlocation()
 
 
-# try number 2
+# cool function name i know
 def do_project(attendee_a, hotels_h):
+    # tried to use x but i had yellow underline that said matches outer scope
+    # and we all know i cant have any yellow underlines
+    # outer loop is going through each hotel name in the sorted list
     for q in attendee_a.sorted_hotel_list:
+        # if the attendee is already in a hotel, break out of loop
         if attendee_a.current_hotel is not None:
             break
         else:
+            # inner loop cycles through each hotel
             for y in hotels_h:
-                if y.num_guests < 16:
+                # if the hotel 14 or less guests, then we can add a guest
+                if y.num_guests < 15:
+                    # if the outer loop hotel name matches the inner loop hotel name
+                    # we add the guest, and give the guest a current hotel
+                    # we then break out of loop
                     if q == y.h_name:
                         y.add_guest(attendee_a.name)
                         attendee_a.current_hotel = y.h_name
                         break
                 else:
+                    # if no num guests is 15 or more, go through next iteration of hotel
                     pass
 
 
+# finding euclidean distance of guest to all hotels
 def dist(attendee_a, hotels_h):
     for q in hotels_h:
         dist1 = math.sqrt(abs(attendee_a.x_val - q.hotel_x) + (abs(attendee_a.y_val - q.hotel_y)))
+        # place in a dictionary, 'Hotel Kevin': 3.456
         attendee_a.hotel_list[q.h_name] = dist1
+    # make sorted dictionary from unsorted dictionary
     attendee_a.sort_hotels(attendee_a.hotel_list, attendee_a.sorted_hotel_list)
 
 
@@ -99,31 +45,24 @@ if __name__ == '__main__':
     # list for hotels class
     hotels = []
 
-    # creating list of hotels
+    # creating 20 of hotels
     for x in range(20):
         hotels.append(Hotel(random.randint(0, 200), random.randint(0, 200), 0, [], 'Hotel ' + names.get_first_name(gender='female')))
 
-    a = Attendee(random.randint(0,200), random.randint(0,200), names.get_first_name(gender='male'), {}, {}, None)
+    # creating 150 of attendees
     attendees = []
     for x in range(150):
         attendees.append(Attendee(random.randint(0,200), random.randint(0,200), names.get_first_name(gender='male'), {}, {}, None))
-    for x in hotels:
-        distance = math.sqrt(abs(a.x_val - x.hotel_x) + (abs(a.y_val - x.hotel_y)))
-        a.hotel_list[x.h_name] = distance
 
-    print(a.hotel_list)
-    a.sort_hotels(a.hotel_list, a.sorted_hotel_list)
-    print(a.sorted_hotel_list)
-
+    # getting distance of all attendees to hotels
     for x in attendees:
         dist(x, hotels)
-        do_project(a, hotels)
+    # placing attendees in hotels
+    for x in attendees:
+        do_project(x, hotels)
+
+    # displaying hotel names and all guests
     for x in hotels:
         print(x.h_name)
         print(x.num_guests)
         print(x.guests)
-
-    # sorted_keys = sorted(a.hotel_list, key=a.hotel_list.get)
-    # for x in sorted_keys:
-    #     a.sorted_hotel_list[x] = a.hotel_list[x]
-    # print(a.sorted_hotel_list)
